935aa1db0978e0831036f0e64f5623063bed48c2
wq <smilingwang2@gmail.com>
2019-03-03 23:55:56 +0800

    exercise solution-1.c

---
+++ chapter5/exercise/solution-1.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: solution-1.c
+	> Author: 
+	> Mail: 
+	> Created Time: Sat Mar  2 10:13:53 2019
+ ************************************************************************/
+
+#include <stdio.h>
+/* Write a program that converts time in minutes to time in hours and minutes. Use #define or const to create a symbolic constant for 60. Use a while loop to allow the user to enter values repeatedly and terminate the loop if a value for the time of 0 or less is entered.
+*/
+#define MINUTE_PER_HOUR 60
+int main(void) {
+    int minutes;
+
+    printf("Please enter minutes:\n");
+    scanf("%d", &minutes);
+    
+    while (minutes > 0) {
+        printf("hours = %d and minutes = %d\n", minutes / MINUTE_PER_HOUR, minutes % MINUTE_PER_HOUR);
+        printf("Please enter minutes:\n");
+        scanf("%d", &minutes);
+    }
+    printf("exit\n");
+}
