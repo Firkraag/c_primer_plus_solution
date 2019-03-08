bf4a3b115faa22ed973aa8634b71fc5445a7806b
wq <smilingwang2@gmail.com>
2019-03-09 00:18:08 +0800

    solution-4.c

--- chapter5/exercise/solution-4.c
+++ chapter5/exercise/solution-4.c
@@ -10,10 +10,25 @@
 /* Write a program that asks the user to enter a height in centimeters and then displays the height in centimeters and in feet and inches. Fractional centimeters and inches should be allowed, and the program should allow the user to continue entering heights until a nonpositive value is entered. A sample run should look like this:
 Enter a height in centimeters: 182
 182.0 cm = 5 feet, 11.7 inches
-Enter a height in centimeters (<=0 to quit): 168.7 168.0 cm = 5 feet, 6.4
-inches
-Enter a height in centimeters (<=0 to quit): 0 bye
+Enter a height in centimeters (<=0 to quit): 168.7 
+168.0 cm = 5 feet, 6.4 inches
+Enter a height in centimeters (<=0 to quit): 0 
+bye
 */
 
 int main(void) {
+    double height;
+    const double CENTIMETERS_PER_FOOT = 30.48;
+    const double CENTIMETERS_PER_INCH = 2.54;
+
+    printf("Enter a height in centimeters (<=0 to quit): ");
+    scanf("%lf", &height);
+    while (height > 0) {
+        int feet =  height / CENTIMETERS_PER_FOOT;
+        double inches = (height - feet * CENTIMETERS_PER_FOOT) / CENTIMETERS_PER_INCH;
+        printf("%lf cm = %d feet, %.1lf inches\n", height, feet, inches); 
+        printf("Enter a height in centimeters (<=0 to quit): ");
+        scanf("%lf", &height);
+    }
+
 }
