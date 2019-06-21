ce191921ff49cc675ffad5e569a439df0c7692ad
wq <smilingwang2@gmail.com>
2019-06-21 16:29:39 +0800

    lethead1.c

---
+++ chapter9/lethead1.c
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> File Name: lethead1.c
+	> Author: 
+	> Mail: 
+	> Created Time: Thu Jun 20 20:09:52 2019
+ ************************************************************************/
+
+#include <stdio.h>
+#include <iso646.h>
+#define NAME "GIGATHINK, INC."
+#define ADDRESS "101 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define WIDTH 40
+
+void starbar(void);
+  
+
+int main(void) {
+    starbar();
+    printf("%s\n", NAME);
+    printf("%s\n", ADDRESS);
+    printf("%s\n", PLACE);
+    starbar();
+
+    return 0;
+
+}
+
+void starbar(void) {
+    int count;
+
+    for (count = 1;count <= WIDTH;count++)
+    {
+        putchar('*');
+    }
+    putchar('\n');
+}
